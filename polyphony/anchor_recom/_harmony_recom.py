3d23130020b244e9d87ec5c70578e1fb40919014
ChengFR <459763742@qq.com>
2022-03-10 20:03:11 +0800

    feat: update anchor recommender functions

--- polyphony/anchor_recom/_harmony_recom.py
+++ polyphony/anchor_recom/_harmony_recom.py
@@ -8,18 +8,14 @@ from polyphony.anchor_recom import AnchorRecommender
 
 class HarmonyAnchorRecommender(AnchorRecommender):
 
-    def recommend_anchors(self, **kwargs):
+    def _recommend_anchors(self, **kwargs):
         hm = run_harmony(
             np.concatenate([self._ref.latent, self._query.latent]),
             pd.concat([self._ref.obs, self._query.obs]),
             'source',
-            max_iter_harmony=0,
-            nclust=20,
             **kwargs
         )
 
         n_ref = len(self._ref.obs)
         self._ref.anchor_mat = hm.R[:, :n_ref].T
         self._query.anchor_mat = hm.R[:, n_ref:].T
-        self._ref.anchor_cluster = self._ref.anchor_mat.argmax(axis=1).astype('str')
-        self._query.anchor_cluster = self._query.anchor_mat.argmax(axis=1).astype('str')
--- polyphony/anchor_recom/_recom.py
+++ polyphony/anchor_recom/_recom.py
@@ -1,19 +1,35 @@
+import numpy as np
+
 from abc import ABC, abstractmethod
-from typing import Optional
 
 from polyphony.dataset import QueryDataset, ReferenceDataset
+from polyphony.utils.math import cluster_agg
 
 
 class AnchorRecommender(ABC):
 
     def __init__(
         self,
-        ref_dataset: Optional[ReferenceDataset] = None,
-        query_dataset: Optional[QueryDataset] = None
+        ref_dataset: ReferenceDataset,
+        query_dataset: QueryDataset
     ):
         self._query = query_dataset
         self._ref = ref_dataset
 
-    @abstractmethod
     def recommend_anchors(self, *args, **kwargs):
+        self._recommend_anchors(*args, **kwargs)
+
+        # assign cluster ids to cell and query data
+        self._ref.anchor_cluster = self._ref.anchor_mat.argmax(axis=1).astype('str')
+        self._query.anchor_cluster = self._query.anchor_mat.argmax(axis=1).astype('str')
+
+        # calculate the distance of each query cell to its corresponding reference cluster's center
+        ref_stat = cluster_agg(self._ref.latent, self._ref.anchor_mat.T)
+        ref_mean = ref_stat[ref_stat.columns[::2]].fillna(0)
+
+        anchor_center = np.dot(self._query.anchor_mat[:,  ref_mean.index], ref_mean)
+        self._query.anchor_dist = np.linalg.norm(self._query.latent - anchor_center, axis=1)
+
+    @abstractmethod
+    def _recommend_anchors(self, *args, **kwargs):
         pass
