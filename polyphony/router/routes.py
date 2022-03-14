faa51fce254f1774ed4f91dec5cc147e1b4b002d
ChengFR <459763742@qq.com>
2022-03-15 02:11:04 +0800

    feat: add model update api

--- polyphony/router/routes.py
+++ polyphony/router/routes.py
@@ -1,4 +1,4 @@
-from flask import send_from_directory
+from flask import current_app, send_from_directory
 from flask_restful import Api
 
 import polyphony.router.resources as res
@@ -14,3 +14,7 @@ def add_routes(app, static_dir):
         return send_from_directory(static_dir, filename=path, as_attachment=True)
 
     api.add_resource(res.AnchorResource, API + 'anchor')
+
+    @app.route('/api/model_update')
+    def model_update():
+        current_app.pm.update_round()
