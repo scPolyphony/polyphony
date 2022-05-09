3858d83b72580ec14ee600296b8a270a488fa51b
ChengFR <459763742@qq.com>
2022-05-09 13:27:21 +0800

    formulate Model apis as resources

--- polyphony/router/routes.py
+++ polyphony/router/routes.py
@@ -1,34 +1,16 @@
-import os
-
-from flask import current_app, send_from_directory
+from flask import send_from_directory
 from flask_restful import Api
 
 import polyphony.router.resources as res
-from polyphony import Polyphony
-from polyphony.tool._projection import umap_transform
 
 API = '/api/'
 
 
-def add_routes(app, static_dir):
+def add_routes(app):
     api = Api(app)
 
-    @app.route('/files/<path:path>')
-    def get_file(path):
-        return send_from_directory(static_dir, filename=path, as_attachment=True)
-
+    # anchor apis
     api.add_resource(res.AnchorResource, API + 'anchor')
 
-    @app.route('/api/model_update')
-    def model_update():
-        pp: Polyphony = current_app.pp
-        zarr_folder = current_app.folders['zarr']
-
-        pp.setup_anndata_anchors(pp.confirmed_anchors)
-        pp.model_update_step()
-        umap_transform(pp.ref, pp.qry)
-        app.pp.recommend_anchors()
-
-        pp.ref.adata.write_zarr(os.path.join(zarr_folder, 'reference.zarr'))
-        pp.qry.adata.write_zarr(os.path.join(zarr_folder, 'query.zarr'))
-        return 'success'
+    # model apis
+    api.add_resource(res.ModelResource, API + 'model')
